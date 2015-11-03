07e61cb74036dcbbb75d573b27ce0cd1411de1fd
Anuj Kumar <anashkan123@gmail.com>
2015-11-03 13:55:46 +0000

    validation of the fields and the presence of the fields maintained

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,19 +1,19 @@
 class Artist < ActiveRecord::Base
-    validates :name, length: { maximum: 100 }
-    validates :email, length: { maximum: 100 }
-    validates :password, length: { maximum: 100 } 
+    validates :name, length: { maximum: 100 } , presence: true
+    validates :email, length: { maximum: 100 } , presence: true
+    validates :password, length: { maximum: 100 } , presence: true
     validates :gravatar_url, length: { maximum: 2083 }
-    validates :description, length: { maximum: 200 }
-    validates :gravatar_url, length: { maximum: 2083 }
-    validates :skill, length: { maximum: 200 }
-    validates :experience, inclusion: { in: 0..50 }
+    validates :description, length: { maximum: 200 } , presence: true
+    
+    validates :skill, length: { maximum: 200 } , presence: true
+    validates :experience, inclusion: { in: 0..50 } , presence: true
     validates :facebook_url, length: { maximum: 2083 }
     validates :twitter_url, length: { maximum: 2083 }
     validates :contact_no, length: { maximum: 15 }
     validates :address, length: { maximum: 300 }
-    validates :country, length: { maximum: 100 }
-    validates :state, length: { maximum: 100 }
-    validates :city, length: { maximum: 100 }
+    validates :country, length: { maximum: 100 }, presence: true
+    validates :state, length: { maximum: 100 }, presence: true
+    validates :city, length: { maximum: 100 }, presence: true
     validates :availability, length: { maximum: 100 }
     validates :youtube_url, length: { maximum: 2083 }
     validates :behance_url, length: { maximum: 2083 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    validates :name, length: { maximum: 100 }
-    validates :email, length: { maximum: 100 }
-    validates :password, length: { maximum: 100 }
+    validates :name, length: { maximum: 100 } , presence: true
+    validates :email, length: { maximum: 100 } , presence: true
+    validates :password, length: { maximum: 100 } , presence: true
     
 end
