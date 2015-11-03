6617250dfa1a5b838a2adc36a7cbc0bbbbf36da1
Anuj Kumar <anashkan123@gmail.com>
2015-11-03 13:40:26 +0000

    User models validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    validates :name, length: { maximum: 100 }
+    validates :email, length: { maximum: 100 }
+    validates :password, length: { maximum: 100 }
+    
 end
