a2621aa54a5f406a2c8d334b5e501157dec1b753
Anuj Kumar <anashkan123@gmail.com>
2015-11-05 14:24:45 +0000

    Finish Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,34 +1,45 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Artisto"
+  end
+  
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title" , "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title" , "Help | #{@base_title}"
   end
 
   test "should get contact_us" do
     get :contact_us
     assert_response :success
+    assert_select "title" , "Contact Us | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title" , "About | #{@base_title}"
   end
 
   test "should get advice" do
     get :advice
     assert_response :success
+    assert_select "title" , "Advice | #{@base_title}"
   end
 
   test "should get search_result" do
     get :search_result
     assert_response :success
+    assert_select "title" , "Search Results | #{@base_title}"
   end
 
 end
