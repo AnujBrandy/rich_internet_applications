207dae632f9baa56bedd154cd7fdf0a1a44d2b14
Anuj Kumar <anashkan123@gmail.com>
2015-11-05 14:40:03 +0000

    Creating a full title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    
+    # returns the full title on a perp page basis
+    def full_title( page_title = '' )
+        base_title = "Artisto"
+        
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
+    
 end
+
